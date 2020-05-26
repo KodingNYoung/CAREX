7c19494b1c904aee8ccb659ceb219fc8f39a15eb
Young <abiodunadebambo44@gmail.com>
2020-05-26 01:40:39 +0100

    added some new features

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-document.querySelector(".burger").addEventListener("click", activateNAv)
-
-
-function activateNAv(){
+const activateNAv = () => {
     document.querySelector(".link-container").classList.toggle("nav-link-show");
 
     document.querySelector(".burger").classList.toggle("toggle");
 }
 
+const fullProgressBar = () => {
+    document.querySelector(".progress").style.width = "100%";
+}
 
+const emptyProgressBar = () => {
+    document.querySelector(".progress").style.width = "7%";
+}
+document.querySelector(".burger").addEventListener("click", activateNAv)
+
+document.querySelector(".findacar").addEventListener("mouseenter", fullProgressBar);
+document.querySelector(".findacar").addEventListener("mouseleave", emptyProgressBar)
